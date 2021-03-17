c4e77ba1866dee52c7e92e1d5e9d9e4c6f6fa873
Viv Richards <1014468+vivrichards600@users.noreply.github.com>
2021-03-17 09:21:40 +0000

    PLATUI-318 - Fixing axe export and including one for vnu for versions

--- docker/files/start.sh
+++ docker/files/start.sh
@@ -2,7 +2,8 @@
 
 # Set the axe-core version for the report
 cd ${HOME}/accessibility_tools
-export NPM_AXE_VERSION=$(npm ls | grep axe-core | head -1 | awk -F@ '{print $2}') \
+export NPM_AXE_VERSION=$(npm ls | grep axe-core | head -1 | awk -F@ '{print $3}') \
+export NPM_VNU_VERSION=$(vnu --version)
 
 # Start the application
 cd ${HOME}/app
