67bc54d2ccf581e0f4370fb290d1194a06a8ec2c
Viv Richards <1014468+vivrichards600@users.noreply.github.com>
2021-06-10 15:57:54 +0100

    PLATUI-1200 - Refactoring reset test and fixing initialise test description

--- accessibility-assessment-service/test/unit/services/globals.test.js
+++ accessibility-assessment-service/test/unit/services/globals.test.js
@@ -10,7 +10,7 @@ describe('applicationStatus', () => {
 
 describe('initialiseApp', () => {
 
-    it('should reset the app', () => {
+    it('should initialise the app', () => {
         initialiseApp('example-accessibility-tests', 'https://build.org.uk');
         expect(global.testSuite).toEqual('example-accessibility-tests');
         expect(global.buildUrl).toEqual('https://build.org.uk');
@@ -18,10 +18,12 @@ describe('initialiseApp', () => {
 });
 
 describe('reset', () => {
-
-    it('should initialise the app', () => {
-        reset();
+    it('should reset the app', () => {
+        reset()
+        expect(global.testSuite).toEqual("not-set");
+        expect(global.buildUrl).toEqual('');
+        expect(global.capturedUrls).toEqual([]);
+        expect(global.excludedUrls).toEqual([]);
         expect(global.status).toEqual('READY');
-        expect(global.testSuite).toEqual('not-set');
     });
 });
