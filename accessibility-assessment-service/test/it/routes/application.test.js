08b0e60074b558764dd4b83f1699d1a6dd305ad8
ejayaraman <7340261+ejayaraman@users.noreply.github.com>
2021-10-25 18:12:24 +0100

    PLATUI-1235: Test error scenarios for `/initialise` and `/global-filters`

--- accessibility-assessment-service/test/it/routes/application.test.js
+++ accessibility-assessment-service/test/it/routes/application.test.js
@@ -27,4 +27,35 @@ describe('application', () => {
         expect(res.statusCode).toEqual(201)
         expect(fs.existsSync(config.globalFilterLocation)).toBe(true);
     });
+
+    it("should error when configuration file is not attached", async () => {
+        const res = await request(app)
+            .post('/api/app/global-filters')
+        expect(res.statusCode).toEqual(500)
+        expect(res.text).toContain("Cannot read property")
+    });
+
+    it("should error when attachment 'key' is not named 'filter'", async () => {
+        const res = await request(app)
+            .post('/api/app/global-filters')
+            .attach('global-filter', path.join(config.testResourcesDir, 'test-global-filters.conf'))
+        expect(res.statusCode).toEqual(400)
+        expect(res.text).toContain("No filter file present in request.")
+    });
+
+    it("should error when initialised without testSuite ", async () => {
+        const res = await request(app)
+            .post("/api/app/initialise")
+            .set('Content-Type', 'application/json')
+            .send({buildUrl: "https://dd.t.uk/"});
+        expect(res.statusCode).toEqual(400)
+    });
+
+    it("should error when initialised without buildUrl ", async () => {
+        const res = await request(app)
+            .post("/api/app/initialise")
+            .set('Content-Type', 'application/json')
+            .send({testSuite: "awesome-tests-a11y-tests"});
+        expect(res.statusCode).toEqual(400)
+    });
 });
