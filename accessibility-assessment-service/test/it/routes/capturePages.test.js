53b0c05b4a57b588722ac157c7c0f20a5255d921
Viv Richards <1014468+vivrichards600@users.noreply.github.com>
2021-11-08 15:55:26 +0000

    PLATUI-1283 - Addding logic to not capture js assets and supporting test.

--- accessibility-assessment-service/test/it/routes/capturePages.test.js
+++ accessibility-assessment-service/test/it/routes/capturePages.test.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const request = require('supertest')
 const {reset} = require('./../../../app/services/globals')
+const fs = require('fs')
+const path = require("path");
+const config = require('./../../../app/config')
 
 describe('capturePage', () => {
     let app;
@@ -89,5 +92,23 @@ describe('capturePage', () => {
         expect(res.statusCode).toEqual(201)
         expect(global.status).toEqual('READY')
         expect(global.excludedUrls).toEqual(["http://localhost:1234/simple/page/capture"])
+    });
+
+    it("should not capture js assets for a page", async () => {
+        const res = await request(app)
+            .post('/api/capture-page')
+            .set('Content-Type', 'application/json')
+            .send({
+                pageURL: "http://localhost:1234/simple/page/capture",
+                pageHTML: "<html><head><title>Some title</title></head><main>The contents of the page</main></html>",
+                timestamp: "0000000002",
+                files: {"file1.js": "some contents"}
+            })
+        expect(res.statusCode).toEqual(201)
+        expect(global.status).toEqual('PAGES_CAPTURED')
+
+        const pageDirectory = path.join(config.pagesDirectory, '' + "0000000002")
+        expect(fs.existsSync(path.join(pageDirectory, "index.html"))).toBe(true);
+        expect(fs.existsSync(path.join(pageDirectory, "file1.js"))).toBe(false);
     });
 })
