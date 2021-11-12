a663dc308f984e7569c64164d6bac1d80b196a7e
ejayaraman <7340261+ejayaraman@users.noreply.github.com>
2021-11-12 12:05:42 +0000

    PLATUI-1341: Determine Axe and VNU version in page-accessibility-check.
    - page-accessibility-check.jar updated to determine AXE and VNU version.
    - Update start.sh to remove existing steps to determine Axe and VNU version

--- docker/files/start.sh
+++ docker/files/start.sh
@@ -1,12 +1,5 @@
 #!/usr/bin/env bash
 
-# Set the axe-core version for the report
-cd ${HOME}/accessibility-assessment-service
-# `npm ls --depth=0 | grep axe-core@` returns `├── axe-core@x.x.x`.
-# `awk -F@ '{print $2}'` returns x.x.x as version number
-export NPM_AXE_VERSION=$(npm ls --depth=0 | grep axe-core@ | awk -F@ '{print $2}') \
-export NPM_VNU_VERSION=$(vnu --version)
-
 # Start the application
 cd ${HOME}/accessibility-assessment-service/app
 export NODE_ENV=docker && node app.js
